467e828aaebf985e127bf0468d655c68a15e44c0
Dan <dchen12812@gmail.com>
2015-10-15 22:18:26 -0700

    added help text

--- Developing Data Products/ui.R
+++ Developing Data Products/ui.R
@@ -11,10 +11,11 @@ shinyUI(fluidPage(
     # Sidebar with a slider input for the number of bins
     sidebarLayout(
         sidebarPanel(
+            helpText("Choose the categories of crimes to display on the map"),
             checkboxGroupInput("checkGroup", 
                                label = h3("Checkbox group"), 
-                               choices = list("Choice 1" = 1, 
-                                              "Choice 2" = 2, "Choice 3" = 3),
+                               choices = list("Choice 1" = 1, "Choice 2" = 2,
+                                              "Choice 2" = 3, "Choice 3" = 4),
                                selected = 1)
         ),
         

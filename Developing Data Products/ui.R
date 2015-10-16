04c2724a6c454cdaa4ddc307710be709ada9a27d
Dan <dchen12812@gmail.com>
2015-10-15 22:19:02 -0700

    renamed choices

--- Developing Data Products/ui.R
+++ Developing Data Products/ui.R
@@ -14,8 +14,8 @@ shinyUI(fluidPage(
             helpText("Choose the categories of crimes to display on the map"),
             checkboxGroupInput("checkGroup", 
                                label = h3("Checkbox group"), 
-                               choices = list("Choice 1" = 1, "Choice 2" = 2,
-                                              "Choice 2" = 3, "Choice 3" = 4),
+                               choices = list("Warrants" = 1, "Vandalism" = 2,
+                                              "Prostitution" = 3, "Bribery" = 4),
                                selected = 1)
         ),
         

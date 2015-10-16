a5779bbfd2c64ddfeb7192c6d6d1ec3568686363
Dan <dchen12812@gmail.com>
2015-10-15 22:16:34 -0700

    added filler checkbox

--- Developing Data Products/ui.R
+++ Developing Data Products/ui.R
@@ -5,17 +5,17 @@ library(shiny)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
     
-    # Application title
-    titlePanel("Hello Shiny!"),
+    # Title
+    titlePanel("San Francisco Crime Classifier"),
     
     # Sidebar with a slider input for the number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            checkboxGroupInput("checkGroup", 
+                               label = h3("Checkbox group"), 
+                               choices = list("Choice 1" = 1, 
+                                              "Choice 2" = 2, "Choice 3" = 3),
+                               selected = 1)
         ),
         
         # Show a plot of the generated distribution

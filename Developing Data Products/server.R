3302a6718f7fd1c3939db1d03412dbe8252cf159
Dan <dchen12812@gmail.com>
2015-10-16 22:53:32 -0700

    added overview and changed it so you can subset by year as well

--- Developing Data Products/server.R
+++ Developing Data Products/server.R
@@ -1,6 +1,5 @@
 #This app will utilize subsetted training data from Kaggle Competition - San Francisco Crime Classification
 
-
 library(shiny)
 
 # Define server logic required for the app
@@ -9,23 +8,29 @@ shinyServer(function(input, output) {
     library(ggmap)
     library(ggplot2)
     library(dplyr)
+    library(lubridate)
     
     #Initializes data
     data = read.csv("subset.csv")
 
+    #Convert dates into year value
+    data$Dates = year(data$Dates)
+    
+    #Setup maps of San Francisco
     map = get_map(location="sanfrancisco",zoom=12,source="osm")
         
     output$locPlot = renderPlot({
-        map_crime = function(crime_df, crime) {
+        map_crime = function(crime_df, crime, year) {
             filtered = filter(crime_df, Category %in% crime)
+            filtered = filter(filtered, Dates %in% year)
             plot = ggmap(map, extent='device') + 
-                geom_point(data = filtered, aes(x = X, y = Y, color=Category, alpha=0.5))
+                geom_point(data = filtered, aes(x = X, y = Y, color=Category))
             return(plot)
         }
         
         #Plots based on crimes selected in checkGroup
         if(length(input$checkGroup) != 0)
-            map_crime(data, c(input$checkGroup))
+            map_crime(data, c(input$checkGroup), input$range)
         else{
             ggmap(map, extent='device')
         }
--- Developing Data Products/ui.R
+++ Developing Data Products/ui.R
@@ -6,22 +6,29 @@ library(shiny)
 shinyUI(fluidPage(
     
     # Title
-    titlePanel("San Francisco Crime Classifier"),
+    titlePanel("San Francisco Crime Map"),
     
     # Sidebar with a slider input for the number of bins
     sidebarLayout(
         sidebarPanel(
-            helpText("Choose the categories of crimes to display on the map"),
+            helpText("Choose the categories of crimes to display on the map as well as the year"),
             checkboxGroupInput("checkGroup", 
                                label = h3("Crime Categories"), 
                                choices = list("Warrants" = 'WARRANTS', "Vandalism" = 'VANDALISM',
                                               "Prostitution" = 'PROSTITUTION', "Bribery" = 'BRIBERY'),
-                               selected = c('WARRANTS','VANDALISM','PROSTITUTION','BRIBERY'))
+                               selected = c('WARRANTS','VANDALISM','PROSTITUTION','BRIBERY')),
+            sliderInput("range", 
+                        label = "Year of interest:",
+                        min = 2003, max = 2015, value = 2003)
         ),
         
         # Show a plot of the generated distribution of crimes on a map of SF
         mainPanel(
-            plotOutput("locPlot")
+            plotOutput("locPlot"),
+            h3('Overview'),
+            p('This uses subsetted data from the Kaggle Competition, San Francisco Crime Classifcation. This app is a
+              used for a quick visualization of four of the crime types listed in the data. You can change the
+              crimes listed in the map as well as the year where the crimes occured.')
         )
     )
 ))

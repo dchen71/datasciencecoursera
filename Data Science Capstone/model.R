2e484e3b0f76bfafdf80bb535d75fc63ba9815f4
Dan <dchen12812@gmail.com>
2015-12-09 14:15:35 -0800

    added samplling to twitter data

--- Data Science Capstone/model.R
+++ Data Science Capstone/model.R
@@ -7,5 +7,6 @@ train_blogs = readLines(paste0(dir,"en_US.blogs.txt"))
 train_news = readLines(paste0(dir,"en_US.news.txt"))
 train_twitter = readLines(paste0(dir,"en_US.twitter.txt"))
 
+train_blogs = sample(train_blogs, 300000)
 
-
+train_twitter = sample(train_twitter, 300000)

c57b39f3b351aeeb713372d5009054a6b1e84899
Dan <dchen12812@gmail.com>
2015-12-09 23:46:16 -0800

    changed the way train news is read

--- Data Science Capstone/model.R
+++ Data Science Capstone/model.R
@@ -4,12 +4,12 @@ library(tm)
 Sys.setlocale(category="LC_ALL", locale = "English_United States.1252")
 
 #Load Data
-##Manually removed strange encoding(sub?) at lines 77259,766277
 dir = 'input/en_US/'
 train_blogs = readLines(paste0(dir,"en_US.blogs.txt"))
-train_news = readLines(paste0(dir,"en_US.news.txt"))
 train_twitter = readLines(paste0(dir,"en_US.twitter.txt"))
-train_news = train_news[1:926142]
+con = file(paste0(dir,"en_US.news.txt"), open="rb")
+train_news = readLines(con, encoding="UTF-8")
+close(con)
 
 train_blogs = sample(train_blogs, 300000)
 train_news = sample(train_news, 300000)

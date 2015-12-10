b3dbdea93deb6dee858d333bd7d3315c4edca676
Dan <dchen12812@gmail.com>
2015-12-09 17:16:51 -0800

    updated sampling of news data

--- Data Science Capstone/model.R
+++ Data Science Capstone/model.R
@@ -1,12 +1,16 @@
 #Load Packages
 library(tm)
 
+Sys.setlocale(category="LC_ALL", locale = "English_United States.1252")
+
 #Load Data
+##Manually removed strange encoding(sub?) at lines 77259,766277
 dir = 'input/en_US/'
 train_blogs = readLines(paste0(dir,"en_US.blogs.txt"))
 train_news = readLines(paste0(dir,"en_US.news.txt"))
 train_twitter = readLines(paste0(dir,"en_US.twitter.txt"))
+train_news = train_news[1:926142]
 
 train_blogs = sample(train_blogs, 300000)
-
+train_news = sample(train_news, 300000)
 train_twitter = sample(train_twitter, 300000)

0e0eec3fff7e93a1d8a24d5ac5be9eb9f3929e00
dchen71 <dchen12812@gmail.com>
2016-01-07 17:32:57 -0800

    split 1gram into own function

--- Data Science Capstone/shinyapp/server.R
+++ Data Science Capstone/shinyapp/server.R
@@ -10,17 +10,7 @@ pred_word = function(word){
   #Nice to have predict completely based on preceding characters
   #could be improved by trying to do local alignment
   if(query_length == 1){
-    if(words$query %in% ngram1_raw$words){ #If word in dict, then it is correct
-      prediction = word
-    }
-    else if(substr(words$query[1],1,1) %in% ngram1_raw$start){ #Takes the highest occuring same starting char
-      start_test = substr(words$query[1],1,1)
-      start_match = ngram1_raw[ngram1_raw$start == start_test,]
-      prediction = as.vector(start_match$words[which.max(start_match$total)])
-    }
-    else{ #Take the highest occuring word, be great if this took it based on local alignment although be less of issue if bigger data size
-      prediction = as.vector(ngram1_raw$words[which.max(ngram1_raw$total)])
-    }
+    prediction = n1_pred(words, word)
   }
   else if(query_length == 2){
     if(words[1,1] %in% ngram2_raw$word1){ #If first word there, take the highest occuring 2nd word following it
@@ -31,6 +21,9 @@ pred_word = function(word){
         word_match = ngram2_raw[ngram2_raw$word1 == words[1,1],]
         prediction = paste(words[1,1], as.vector(word_match$word2[which.max(word_match$total)]))
       }
+    }
+    else if(words[1,1] %in% ngram1_raw$words){
+      #predict 1gram
     }
     else{ #Take highest occuring preceding word
       word_match = ngram2_raw[ngram2_raw$word1 == as.vector(ngram1_raw$words[which.max(ngram1_raw$total)]),]
@@ -68,6 +61,21 @@ pred_word = function(word){
     prediction = "Please limit prediction to 3 works max"
   }
   
+  return(prediction)
+}
+
+n1_pred = function(words, word){
+  if(words$query %in% ngram1_raw$words){ #If word in dict, then it is correct
+    prediction = word
+  }
+  else if(substr(words$query[1],1,1) %in% ngram1_raw$start){ #Takes the highest occuring same starting char
+    start_test = substr(words$query[1],1,1)
+    start_match = ngram1_raw[ngram1_raw$start == start_test,]
+    prediction = as.vector(start_match$words[which.max(start_match$total)])
+  }
+  else{ #Take the highest occuring word, be great if this took it based on local alignment although be less of issue if bigger data size
+    prediction = as.vector(ngram1_raw$words[which.max(ngram1_raw$total)])
+  }
   return(prediction)
 }
 
